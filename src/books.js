d75c8cc4c793ab4c24139e65f20024934c223889
fahriesa02 <fujinkai02@gmail.com>
2021-06-05 10:04:06 +0800

    modified books.js

--- src/books.js
+++ src/books.js
@@ -1,3 +1,41 @@
-const books = [];
+const {nanoid} = require('nanoid');
 
-module.exports = books;
+class Book {
+  constructor(entry) {
+    this.id = nanoid(16);
+    this.name = entry.name;
+    this.year = entry.year;
+    this.author = entry.author;
+    this.summary = entry.summary;
+    this.publisher = entry.publisher;
+    this.pageCount = entry.pageCount;
+    this.readPage = entry.readPage;
+    this.finished = entry.readPage === entry.pageCount;
+    this.reading = entry.reading;
+
+    const currentTime = new Date().toISOString();
+    this.insertedAt = currentTime;
+    this.updatedAt = currentTime;
+  }
+
+  updateBook(newData) {
+    this.name = newData.name;
+    this.year = newData.year;
+    this.author = newData.author;
+    this.summary = newData.summary;
+    this.publisher = newData.publisher;
+    this.pageCount = newData.pageCount;
+    this.readPage = newData.readPage;
+    this.reading = newData.reading;
+    this.updatedAt = new Date().toISOString();
+  }
+
+  // eslint-disable-next-line require-jsdoc
+  getIdNameAndPublisher() {
+    return {
+      id: this.id, name: this.name, publisher: this.publisher,
+    };
+  }
+}
+
+module.exports = Book;
